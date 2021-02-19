e39fb2e6b2975ffb84211662e21c5a0dbc7405fe
Onibenjo <onibenjo@gmail.com>
2021-02-19 04:37:01 +0100

    feat: stories loaded

--- packages/Components/storybook/stories/index.js
+++ packages/Components/storybook/stories/index.js
@@ -1,2 +1,4 @@
 import './Button/Button.stories';
 import './Welcome/Welcome.stories';
+import '../../src/components/Button/Button.stories';
+import '../../src/components/ImageView/ImageView.stories';
